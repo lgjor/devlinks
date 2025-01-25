88e7104bd1293357fa05dc34140c2a8f04e2817c
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-01-25 18:46:48 -0300

    :tada: projeto finalizado

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ function toggleMode(){
   const html = document.documentElement;
 
 // Muda a classe do HTML entre light e dark
-html.classList.toggle('light');
+html.classList.toggle('dark');
 
 // Pegar a tag img do perfil
 const img = document.querySelector('#profile img');
 
 // Substituir a img
-if (html.classList.contains('light')) {
-  img.setAttribute('src', './assets/images/avatar-light.png');
+if (html.classList.contains('dark')) {
+  img.setAttribute('src', './assets/images/avatar-dark.png');
 } else {
   img.setAttribute('src', './assets/images/avatar.png');
 }
